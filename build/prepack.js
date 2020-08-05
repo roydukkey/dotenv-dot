1688e20f6bed9277a588a0ab9fd5aca9b31ad01d
roydukkey <contact@changelog.me>
2020-08-05 17:30:30 -0400

    Add variables for strings in prepack script

--- build/prepack.js
+++ build/prepack.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 const package = require('../package.json');
+const file = './package.json';
+const backup = `${file}.build`;
 
 // Backup source file
-fs.renameSync('./package.json', './package.json.build');
+fs.renameSync(file, backup);
 
 // Delete keys which are not needed in release package
 package.releasePackageOmit.forEach((value) => delete package[value]);
 
 // Write changes to package
-fs.writeFileSync('./package.json', JSON.stringify(package, null, '\t'));
+fs.writeFileSync(file, JSON.stringify(package, null, '\t'));
